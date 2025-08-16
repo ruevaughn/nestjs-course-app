72fd7376d8857c319d12ba62c25bc3e6d3b9476c
Chase Jensen <soccerchase@gmail.com>
2025-08-16 08:47:33 -0600

    Add Event Entity

--- src/events/entities/event.entity.ts
+++ src/events/entities/event.entity.ts
@@ -1,1 +1,16 @@
-export class EventEntity {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Event {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  type: string;
+
+  @Column()
+  name: string;
+
+  @Column('json')
+  payload: Record<string, any>;
+}
