eebf1cfe37f467406bcd5b65eba6f5d285e255e6
Chase Jensen <soccerchase@gmail.com>
2025-09-03 08:03:30 -0600

    Change scope to request

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -9,7 +9,7 @@ import { PaginationQueryDto } from '../common/dto/pagination-query.dto';
 import { Event } from '../events/entities/event.entity';
 import { COFFEE_BRANDS } from './coffees.constants';
 
-@Injectable({ scope: Scope.TRANSIENT })
+@Injectable({ scope: Scope.REQUEST })
 export class CoffeesService {
   constructor(
     @InjectRepository(Coffee)
