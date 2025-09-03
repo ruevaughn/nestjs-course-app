07c68e653f186f577e9e1e66d0a23dfa0310cce6
Chase Jensen <soccerchase@gmail.com>
2025-09-03 08:11:31 -0600

    Finish wiring up controller to be request scoped

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -7,15 +7,20 @@ import {
   Delete,
   Body,
   Query,
+  Req,
+  Inject,
 } from '@nestjs/common';
 import { CoffeesService } from './coffees.service';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 import { PaginationQueryDto } from '../common/dto/pagination-query.dto';
-
+import { REQUEST } from '@nestjs/core';
 @Controller('coffees')
 export class CoffeesController {
-  constructor(private readonly coffeeService: CoffeesService) {
+  constructor(
+    private readonly coffeeService: CoffeesService,
+    @Inject(REQUEST) private readonly request: Request,
+  ) {
     console.log('CoffeesController instantiated');
   }
 
